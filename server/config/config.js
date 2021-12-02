f722fb8bdedf2299b90f7256796dccd3a16211e8
dudgks5846 <dudgks5846@naver.com>
2021-12-02 13:42:07 +0900

    [fix] git 충돌해결

--- server/config/config.js
+++ server/config/config.js
@@ -3,62 +3,23 @@ dotenv.config();
 
 module.exports = {
   development: {
-<<<<<<< HEAD
-<<<<<<< HEAD
     username: process.env.DATABASE_USERNAME || "root",
     password: process.env.DATABASE_PASSWORD,
-    //logging: true,
     database: process.env.DATABASE_NAME || "weadresser",
-=======
-    username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME || 'weadresser',
->>>>>>> a7d5c121d0a8fc3d3e411f595c141538c0463a82
-=======
-    username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME || 'weadresser',
->>>>>>> a7d5c121d0a8fc3d3e411f595c141538c0463a82
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
-<<<<<<< HEAD
-<<<<<<< HEAD
     username: process.env.DATABASE_USERNAME || "root",
     password: process.env.DATABASE_PASSWORD,
-    //logging: true,
     database: process.env.DATABASE_NAME || "weadresser",
-=======
-    username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME || 'weadresser',
->>>>>>> a7d5c121d0a8fc3d3e411f595c141538c0463a82
-=======
-    username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME || 'weadresser',
->>>>>>> a7d5c121d0a8fc3d3e411f595c141538c0463a82
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-<<<<<<< HEAD
-<<<<<<< HEAD
     username: process.env.DATABASE_USERNAME || "root",
     password: process.env.DATABASE_PASSWORD,
-    //logging: true,
     database: process.env.DATABASE_NAME || "weadresser",
-=======
-    username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME || 'weadresser',
->>>>>>> a7d5c121d0a8fc3d3e411f595c141538c0463a82
-=======
-    username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME || 'weadresser',
->>>>>>> a7d5c121d0a8fc3d3e411f595c141538c0463a82
     host: "127.0.0.1",
     dialect: "mysql",
   },
