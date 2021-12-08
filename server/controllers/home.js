4a944838a576224c1cd4ca16618c1211a04edac9
minch jung <minch@minchui-MacBookAir.local>
2021-12-09 01:01:10 +0900

    ADD& FIX: 주석 처리 및 설명

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -19,15 +19,15 @@ module.exports = {
             //! DB no data (미리 넣을거라 가능성 희박) BUT 협의
         }        
         else{ // !no random => nodata ? x
-          // DB no data (미리 넣을거라 가능성 희박) BUT 협의
           const topOne = await sequelize.query( topQuery, { raw : true })
           return topOne[0].length > 0
-            ? res.status(202).json([ null, topOne[0][0] ])
-            : res.status(404).json([ null, null ]) 
+          ? res.status(202).json([ null, topOne[0][0] ])
+          : res.status(404).json([ null, null ]) 
+          //! DB no data (미리 넣을거라 가능성 희박) BUT 협의
         }
       })
       .catch( err => {
-        //!Todo error handlring (DB query => request too much then will break )
+        //!Todo error handlring (DB query => too much request,then server will break )
         return res.status(500).send("Internal Server err") 
       })    
   }, 
@@ -39,6 +39,7 @@ module.exports = {
     const userQuery = findLatestById(userId, tempMin, tempMax) // leftOne
     const topQuery = findTopLikeById(userId, tempMin, tempMax); // rightOne
 
+    // find one by userId
     return sequelize.query( userQuery, { raw: true } )
       .then(  async userOne => { 
         //  found user one OK 
@@ -49,21 +50,21 @@ module.exports = {
           ? res.status(200).json([ userOne[0][0], topOne[0][0] ]) 
           : res.status(201).json([ userOne[0][0], null ]) 
           //! DB no data (미리 넣을거라 가능성 희박) BUT 협의
-        }
-        else{ 
+        }     
+        else{ // not any from user
           // * user data 없을 수 있음 
           const randomQuery = findRandomOne(tempMin, tempMax);
-          // Search random one 
+          // find random one  
           return sequelize.query( randomQuery, { raw : true })
           .then( async randomFound => {
             if(randomFound[0].length > 0){
-              // Search most like one
+              // find one by most like
               const topOne = await sequelize.query( topQuery, { raw : true })
               // both OK
               return topOne[0].length > 0  
                 ? res.status(200).json([ randomFound[0][0], topOne[0][0] ])
                 : res.status(201).json([ randomFound[0][0], null ])  
-                // random one OK  no most like
+                // random one OK  not found of the most like
                 //! DB no data (미리 넣을거라 가능성 희박) BUT 협의
             }        
             else{ //! no random ? => no data x  
