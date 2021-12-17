067bd6c5141e01dbc4262a4f6b0ee1bd3ce6f429
minch jung <minch@minchui-MacBookAir.local>
2021-12-17 13:36:40 +0900

    before merge

--- client/src/components/EditRecord/EditRecord.js
+++ client/src/components/EditRecord/EditRecord.js
@@ -74,12 +74,11 @@ function EditRecord({ curSlide, setIsEdit }) {
         formData.append('share', sharePost); 
 
         // const url = process.env.REACT_APP_SERVER_URL || 
-        const url = 'http://localhost:80/diary' // server랑 확인할때 환경변수 x
-        axios.patch(url, formData, {
+        const url = 'http://localhost:80/mypage/diary' // server랑 확인할때 환경변수 x
+        axios.post('http://localhost:80/mypage/diary', formData, { 
             headers: {
                 'content-type': 'multipart/form-data'
-            }},
-            { withCredential: true, })
+            }, withCredentials: true })
             .then(res => console.log('edit successfully'))
             .catch(err => console.log(err))
 
