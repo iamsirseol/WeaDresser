bf070fd6123059d5ddac86b4310ed4986f23e68e
minch jung <minch@minchui-MacBookAir.local>
2021-12-17 10:54:17 +0900

    chore: delete not using import component on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,17 @@
-// import React, { useEffect, useState } from "react";
 import './styles/reset.css';
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-// import { isLoginHandler, isShowLoginModalHandler, isShowSignUpModalHandler } from './redux/actions/actions'
 import LoadingIndicator from './components/Loading/LoadingIndicator'
 import NavBar from "./components/NavBar/NavBar"
-
 import OotdListPage from "./pages/OotdPage/OotdListPage"
-// import {SolidHeart} from "./components/SvgIcon/SvgIcon"
-// import Login from "./components/Modal/slideModal/Login"
-// import Signup from "./components/Modal/slideModal/Signup"
 import LandingPage from './pages/LandingPage/LandingPage';
-import {SolidHeart} from "./components/SvgIcon/SvgIcon"
 import Modal from "./components/Modal/SignModal/Modal"
-// import RecordPage from './pages/RecordPage/RecordPage';
 import MyPage from './pages/MyPage/MyPage'
-import { isLoadingHandler, loginSuccessHandler } from './redux/actions/actions';
-// import styled from 'styled-components';
-import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import RecordPage from './pages/RecordPage/RecordPage';
-import Footer from './components/Footer/Footer'
-import { useCallback, useEffect, useState } from 'react';
-import axios from 'axios';
-import { getWeatherData, getLocationData, tempLoadingHandler } from './redux/actions/actions'
 import { useLoading } from './utils/useLoading'
-import { LoginContainer } from './components/Modal/SignModal/LoginStyle';
 require('dotenv').config();
 
 function App() {
   const { tempLoading, logoutHandler } = useLoading();
-  // console.log("App rendering with ", tempLoading )
 
   return (
     <BrowserRouter>
